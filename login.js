fb19c5994170babc1546fd568a6aeba96c096d86
senda1988 <zidi.senda@gmail.com>
2025-03-05 13:55:28 +0100

    Delete login.js

--- login.js
+++
@@ -1,9 +0,0 @@
-function onlogin() {
-    const emailC = document.getElementById("emailti").value;
-    const passC = document.getElementById("passwordti").value;
-    if (emailC && passC) {
-        alert(`Email: ${emailC} and Password: ${passC}`);
-    } else {
-        alert(`email and pasword incorrect!`);
-    }
-}
